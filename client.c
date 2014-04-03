b67685b04ac7be4ba0c721dede086bb842bdacb3
lee jaehoon <jaehoonlee88@gmail.com>
2014-04-04 02:05:46 +0900

    1st client finished

--- client.c
+++ client.c
@@ -15,19 +15,24 @@ unsigned short checksum(const char *buf, unsigned size);
 
 int main(int argc, char *argv[])
 {
-  int sockfd, portno, n;
+  int sockfd, portno, n, proto_num, i;
   struct sockaddr_in serv_addr;
 
   char negobuf[8];
   char buffer[256];
-  char bufin[256];
+  char strbuffer[256];
 
   if (argc < 7) {
-    fprintf(stderr, "usage %s -h hostname -p port -m 1\n", argv[0]);
+    fprintf(stderr, "usage %s -h hostname -p port -m protocol \n", argv[0]);
     exit(0);
   }
 
   portno = atoi(argv[4]);
+  proto_num = atoi(argv[6]);
+  if((proto_num != 1) && (proto_num != 2)) {
+    perror("wrong protocol number, only 1 and 2 are available");
+    exit(0);
+  }
   
   /* create socket */
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -40,6 +45,7 @@ int main(int argc, char *argv[])
   serv_addr.sin_addr.s_addr = inet_addr(argv[2]);
   serv_addr.sin_port = htons(portno);
 
+
   /* connect */
   if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
     perror("ERROR connecting");
@@ -49,10 +55,9 @@ int main(int argc, char *argv[])
   /* protocol negotiaton */
   readSocket(sockfd, negobuf, 8);
 
-
   /* checksum calculation */
   negobuf[0] = 0x01;
-  negobuf[1] = 0x01;
+  negobuf[1] = proto_num & 0xff;
   negobuf[2] = 0x00;
   negobuf[3] = 0x00;
 
@@ -68,36 +73,75 @@ int main(int argc, char *argv[])
   bzero(buffer, 256);
   fgets(buffer, 255, stdin);
 
-  /* encoding protocol 2-1 */
-  int i=0;
-  while(i < 256){
-
-    if(buffer[i] == 0x00){
-      buffer[i-1] = '\\';
-      buffer[i] = '0';
-      //printf("break:%d\n", i);
-      break;
+  /* encoding protocol */
+  if(proto_num == 1) {    
+    /* encoding protocol 2-1 */
+    i=0;
+    while(i < 256){
+      
+      if(buffer[i] == 0x00){
+	buffer[i-1] = '\\';
+	buffer[i] = '0';
+	//printf("break:%d\n", i);
+	break;
+      }
+      //    printf("%c", buffer[i]);      
+      i++;
     }
-    //    printf("%c", buffer[i]);
-
-    i++;
-  }
-
-  write(sockfd, buffer, sizeof(buffer));
-  readSocket(sockfd, buffer, sizeof(buffer));
-
-  /* decoding protocol 2-1 */
-  while(i < 256){
-    
-    if(buffer[i] == '\\'){
-      buffer[i] = 0x00;
-      buffer[i+1] = 0x00;
-      break;
+  }else if(proto_num == 2) {
+    /* encoding protocol 2-2 */
+    i=0;
+    while(i < 256){      
+      if(buffer[i] == 0x00){
+	break;
+      }
+      i++;
     }
-    i++;
+    strbuffer[3] = i & 0xff;
+    strbuffer[2] = (i >> 8) & 0xff;
+    strbuffer[1] = (i >> 16) & 0xff;
+    strbuffer[0] = (i >> 24) & 0xff;
+    memcpy(strbuffer+4, buffer, 256);
   }
   
-  printbufdump(buffer);
+  /* write string to server */
+  if(proto_num == 1)
+    write(sockfd, buffer, sizeof(buffer));
+  else if(proto_num == 2)
+    write(sockfd, strbuffer, sizeof(strbuffer));
+
+  /* read response from server */
+  if(proto_num == 1)
+    readSocket(sockfd, buffer, sizeof(buffer));
+  else if(proto_num == 2)
+    readSocket(sockfd, strbuffer, sizeof(strbuffer));
+
+
+  /* decoding protocol 2 */
+  i = 0;
+  if(proto_num == 1){    
+    while(i < 256){
+      
+      if(buffer[i] == '\\'){
+	buffer[i] = 0x00;
+	buffer[i+1] = 0x00;
+	break;
+      }
+      i++;
+    }
+  }else if(proto_num == 2){
+    int leng = 0;
+
+    leng = leng + strbuffer[0];
+    leng = (leng << 8) + strbuffer[1];
+    leng = (leng << 8) + strbuffer[2];
+    leng = (leng << 8) + strbuffer[3];
+    
+    bzero(buffer, sizeof(buffer));
+    memcpy(buffer, strbuffer+4, leng);
+  }
+
+
   printf("%s", buffer);
 
   return 0;
